a26c1b8cd9e723e07b4534b8005464e40a5943ad
Luciomar F  Santos <luciomarfsantos@MacBook-Pro-de-Luciomar.local>
2020-09-11 00:10:45 -0300

    commin nr 1

--- src/routes/rotas.jsx
+++ src/routes/rotas.jsx
@@ -4,8 +4,8 @@ import About from '../pages/about/About';
 
 var indexRoutes = [
   
-  { path: "/instrucao61/build/about", name: "About", component: About },
-  { path: "/instrucao61/build/", name: "Home", component: Home}
+  { path: "/about", name: "About", component: About },
+  { path: "/", name: "Home", component: Home}
 ];
 
 export default indexRoutes;
